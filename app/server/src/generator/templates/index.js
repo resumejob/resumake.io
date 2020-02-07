96b929f09a1362614d6b20aadc033940f4d37f99
Windsooon <wiwindson@outlook.com>
2020-02-07 13:12:26 +0800

    delete unused templates

--- app/server/src/generator/templates/index.js
+++ app/server/src/generator/templates/index.js
@@ -3,25 +3,13 @@
  */
 
 import { join } from 'path'
-import template1 from './template1'
 import template2 from './template2'
-import template3 from './template3'
 import template4 from './template4'
-import template5 from './template5'
 import template6 from './template6'
-import template7 from './template7'
-import template8 from './template8'
-import template9 from './template9'
 import {
   TEMPLATE1,
   TEMPLATE2,
   TEMPLATE3,
-  TEMPLATE4,
-  TEMPLATE5,
-  TEMPLATE6,
-  TEMPLATE7,
-  TEMPLATE8,
-  TEMPLATE9
 } from './constants'
 import type { SanitizedValues, TemplateData } from '../../types'
 
@@ -39,14 +27,6 @@ function getTemplateData(data: SanitizedValues): TemplateData {
   switch (data.selectedTemplate) {
     case TEMPLATE1:
       return {
-        texDoc: template1(data),
-        opts: {
-          cmd: 'pdflatex'
-        }
-      }
-
-    case TEMPLATE2:
-      return {
         texDoc: template2(data),
         opts: {
           cmd: 'xelatex',
@@ -55,15 +35,7 @@ function getTemplateData(data: SanitizedValues): TemplateData {
         }
       }
 
-    case TEMPLATE3:
-      return {
-        texDoc: template3(data),
-        opts: {
-          cmd: 'pdflatex'
-        }
-      }
-
-    case TEMPLATE4:
+    case TEMPLATE2:
       return {
         texDoc: template4(data),
         opts: {
@@ -73,16 +45,7 @@ function getTemplateData(data: SanitizedValues): TemplateData {
         }
       }
 
-    case TEMPLATE5:
-      return {
-        texDoc: template5(data),
-        opts: {
-          cmd: 'pdflatex',
-          inputs: join(__dirname, 'template5', 'inputs')
-        }
-      }
-
-    case TEMPLATE6:
+    case TEMPLATE3:
       return {
         texDoc: template6(data),
         opts: {
@@ -92,37 +55,13 @@ function getTemplateData(data: SanitizedValues): TemplateData {
         }
       }
 
-    case TEMPLATE7:
-      return {
-        texDoc: template7(data),
-        opts: {
-          cmd: 'pdflatex',
-          inputs: join(__dirname, 'template7', 'inputs')
-        }
-      }
-
-    case TEMPLATE8:
-      return {
-        texDoc: template8(data),
-        opts: {
-          cmd: 'lualatex',
-          inputs: join(__dirname, 'template8', 'inputs')
-        }
-      }
-
-    case TEMPLATE9:
-      return {
-        texDoc: template9(data),
-        opts: {
-          cmd: 'pdflatex'
-        }
-      }
-
     default:
       return {
-        texDoc: template1(data),
+        texDoc: template2(data),
         opts: {
-          cmd: 'pdflatex'
+          cmd: 'xelatex',
+          inputs: join(__dirname, 'template2', 'inputs'),
+          fonts: join(__dirname, 'template2', 'inputs')
         }
       }
   }
